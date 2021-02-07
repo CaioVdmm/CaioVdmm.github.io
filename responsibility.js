5b84dfbfe0f3d6c568fdc22e77d29229965e6377
CaioVdmm <caiovdmm@gmail.com>
2021-02-07 16:32:30 -0300

    update

---
+++ responsibility.js
@@ -0,0 +1,8 @@
+function collapse(){
+  var menu = document.getElementById("menu2");
+  if (menu.style.display === "block"){
+    menu.style.display = "none";
+  }else{
+    menu.style.display = "block";
+  }
+}
